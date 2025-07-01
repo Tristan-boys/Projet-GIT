eddc29bded9868db2418234351169bf20bcc2ac6
tipsey <tristan.philigan@gmail.com>
2025-07-01 19:50:17 +0200

    fix : you can now win the game

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ buttons.forEach(button => button.addEventListener('click', (event) => {
         changeColors();
     }
     label.innerText = 'score = ' + score;
-}));
-
-label.addEventListener('change', (event) => {
-    if(score == 10) {
+    if(score === 10) {
         document.getElementById('game').classList.add('none');
         label.innerHTML = 'You WIN ! <br><button id="restart">restart</button>';
     }
@@ -27,13 +24,12 @@ label.addEventListener('change', (event) => {
         document.getElementById('game').classList.add('none');
         label.innerHTML = 'You LOOSE ! AHAH don' + "'" + 't cheat <br><button id="restart">restart</button>';
     }
-})
-
-try {
-    document.getElementById('restart').addEventListener('click', event => {
-        document.getElementById('game').classList.remove('none');
-        score = 0;
-        label.innerHTML = 'score = 0';
-    })
-}
-catch (e){}
+    try {
+        document.getElementById('restart').addEventListener('click', event => {
+            document.getElementById('game').classList.remove('none');
+            score = 0;
+            label.innerHTML = 'score = 0';
+        })
+    }
+    catch (e){}
+}));
