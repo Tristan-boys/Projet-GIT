071fa0bbcadf061c7ea73df7d2f13e6a0e247c7a
tipsey <tristan.philigan@gmail.com>
2025-07-01 19:43:36 +0200

    feat : try to make win the game

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ buttons.forEach(button => button.addEventListener('click', (event) => {
     }
     label.innerText = 'score = ' + score;
 }));
+
+label.addEventListener('change', (event) => {
+    if(score == 10) {
+        document.getElementById('game').classList.add('none');
+        label.innerHTML = 'You WIN ! <br><button id="restart">restart</button>';
+    }
+    if(score > 10) {
+        document.getElementById('game').classList.add('none');
+        label.innerHTML = 'You LOOSE ! AHAH don' + "'" + 't cheat <br><button id="restart">restart</button>';
+    }
+})
+
+try {
+    document.getElementById('restart').addEventListener('click', event => {
+        document.getElementById('game').classList.remove('none');
+        score = 0;
+        label.innerHTML = 'score = 0';
+    })
+}
+catch (e){}
